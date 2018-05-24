a84a818c66574f05b0956f32a964c6b43bb0eab5
Sunny-zz <b695340402@163.com>
2018-05-24 16:11:25 +0800

    list style

--- react/show-pic/src/components/Banner.js
+++ react/show-pic/src/components/Banner.js
@@ -27,7 +27,11 @@ class Banner extends Component {
         id: 5,
         src: forthPic
       }
-    ]
+    ],
+    show: 0
+  }
+  handleClick = ind => {
+    console.log(ind)
   }
   render() {
     const { pic } = this.state
@@ -36,9 +40,17 @@ class Banner extends Component {
     const showPic = pic.map(pic => (
       <img style={{ width: imgWidth }} src={pic.src} alt="" key={pic.id} />
     ))
+    const buttonList = pic.map((pic, index) => (
+      <Button key={pic.id} onClick={this.handleClick} />
+    ))
     return (
       <Show>
-        <div style={{ width: picWidth }}>{showPic}</div>
+        <div
+          style={{ width: picWidth, transition: 'margin-left 0.75s linear' }}
+        >
+          {showPic}
+        </div>
+        <List>{buttonList}</List>
       </Show>
     )
   }
@@ -49,4 +61,25 @@ const Show = styled.div`
   width: 80%;
   margin: 100px auto;
   overflow: hidden;
+  position: relative;
+`
+const List = styled.ul`
+  width: 40%;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  position: absolute;
+  display: flex;
+  bottom: 18px;
+  left: 50%;
+  margin-left: -20%;
+  justify-content: space-between;
+`
+const Button = styled.li`
+  width: 25px;
+  height: 25px;
+  background-color: #f1f8f9;
+  opacity: 0.6;
+  border-radius: 50%;
+  cursor: pointer;
 `
