6951f4fecbf25cf849dd75b1d37ef0c487b93b77
Sunny-zz <b695340402@163.com>
2018-05-28 10:31:01 +0800

    ant-modal

--- react-ui/src/components/AntdDemo.js
+++ react-ui/src/components/AntdDemo.js
@@ -1,22 +1,43 @@
 import React, { Component } from 'react'
 // import { DatePicker } from 'antd'
 // import Test from './Test'
-import { Button } from 'antd'
+// import { Button } from 'antd'
+import { Modal } from 'antd'
 
 class AntdDemo extends Component {
+  // state = {
+  //   loading: false
+  // }
+  // enterLoading = () => {
+  //   this.setState({ loading: true })
+  // }
+  // handelClose = () => {
+  //   this.setState({ loading: false })
+  // }
   state = {
-    loading: false
+    visible: false
   }
-  enterLoading = () => {
-    this.setState({ loading: true })
+  showModal = () => {
+    this.setState({
+      visible: true
+    })
   }
-  handelClose = () => {
-    this.setState({ loading: false })
+  handleOk = e => {
+    console.log(e)
+    this.setState({
+      visible: false
+    })
+  }
+  handleCancel = e => {
+    console.log(e)
+    this.setState({
+      visible: false
+    })
   }
   render() {
     return (
       <div>
-        <Button ghost={true} onClick={this.handelClose} type="primary">
+        {/* <Button ghost={true} onClick={this.handelClose} type="primary">
           Default
         </Button>
         <Button
@@ -27,7 +48,18 @@ class AntdDemo extends Component {
         >
           Click me!
         </Button>
-        {/* <Test>111</Test> */}
+        <Test>111</Test> */}
+        <button onClick={this.showModal}>按钮</button>
+        <Modal
+          title="标题"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+          maskClosable={false}
+          okText="确定"
+        >
+          khkjhkjh
+        </Modal>
       </div>
     )
   }
