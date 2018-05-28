98073c0d79bf8ec31627d52c13d3bee01145c7b6
Sunny-zz <b695340402@163.com>
2018-05-28 16:59:43 +0800

    recharts

--- react-ui/src/components/MaterialDemo.js
+++ react-ui/src/components/MaterialDemo.js
@@ -9,22 +9,25 @@ import pink from '@material-ui/core/colors/pink'
 import purple from '@material-ui/core/colors/purple'
 import Switch from '@material-ui/core/Switch'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
+import AppBar from '@material-ui/core/AppBar'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
 class MaterialDemo extends Component {
   state = {
-    checkedA: true,
-    checkedB: true
+    checkedB: true,
+    value: 0
+  }
+  handleChange1 = (event, value) => {
+    this.setState({ value })
   }
 
   handleChange = name => event => {
     this.setState({ [name]: event.target.checked })
   }
   render() {
-    const theme = createMuiTheme({
-      palette: {
-        primary: pink
-      }
-    })
+    const theme = createMuiTheme({ palette: { primary: purple } })
     const { classes } = this.props
+    const { value } = this.state
     return (
       <MuiThemeProvider theme={theme}>
         <Button variant="raised" disableRipple={true} color="primary">
@@ -33,38 +36,43 @@ class MaterialDemo extends Component {
         <Button variant="raised" disableRipple={true} color="secondary">
           按钮
         </Button>
+
         <FormControlLabel
           control={
             <Switch
-              checked={this.state.checkedA}
-              onChange={this.handleChange('checkedA')}
-              value="checkedA"
               classes={{
-                switchBase: classes.colorSwitchBase,
-                checked: classes.colorChecked,
-                bar: classes.colorBar
+                switchBase: classes.iOSSwitchBase,
+                bar: classes.iOSBar,
+                icon: classes.iOSIcon,
+                iconChecked: classes.iOSIconChecked,
+                checked: classes.iOSChecked
               }}
+              disableRipple
+              checked={this.state.checkedB}
+              onChange={this.handleChange('checkedB')}
+              value="checkedB"
             />
           }
-          label="Custom color"
+          label="iOS style"
         />
+        <div>
+          <AppBar position="static">
+            <Tabs value={value} onChange={this.handleChange1}>
+              <Tab label="Item One" />
+              <Tab label="Item Two" />
+              <Tab label="Item Three" href="#basic-tabs" />
+            </Tabs>
+          </AppBar>
+          {value === 0 && <div>Item One</div>}
+          {value === 1 && <div>Item Two</div>}
+          {value === 2 && <div>Item Three</div>}
+        </div>
       </MuiThemeProvider>
     )
   }
 }
 
 const styles = theme => ({
-  colorSwitchBase: {
-    color: purple[300],
-    '&$colorChecked': {
-      color: purple[500],
-      '& + $colorBar': {
-        backgroundColor: purple[500]
-      }
-    }
-  },
-  colorBar: {},
-  colorChecked: {},
   iOSSwitchBase: {
     '&$iOSChecked': {
       color: theme.palette.common.white,
